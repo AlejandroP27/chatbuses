edf8719a11b34da0573f8824d0dfbcd23487221f
Alejandro Perez <alejandroperezb27@gmail.com>
2020-08-20 00:53:30 +0200

    subiendo el anterior server

--- server.js
+++ server.js
@@ -1,32 +1,48 @@
-const SocketServer = require('websocket').server
-const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.static("public"))
+
+const http = require('http').Server(app)
+const serverSocket = require('socket.io')(http)
 
 const porta = process.env.PORT || 8000
-const server = http.createServer((req, res) => {})
 
-server.listen(porta, ()=>{
-    console.log("Listening on port 3000...")
+const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "http://localhost"
+
+http.listen(porta, function(){
+    const portaStr = porta === 80 ? '' :  ':' + porta
+
+    if (process.env.HEROKU_APP_NAME) 
+        console.log('Servidor iniciado. Abra o navegador em ' + host)
+    else console.log('Servidor iniciado. Abra o navegador em ' + host + portaStr)
 })
 
-wsServer = new SocketServer({httpServer:server})
-
-const connections = []
-
-wsServer.on('request', (req) => {
-    const connection = req.accept()
-    console.log('new connection')
-    connections.push(connection)
-
-    connection.on('message', (mes) => {
-        connections.forEach(element => {
-            if (element != connection)
-                element.sendUTF(mes.utf8Data)
-        })
-    })
-
-    connection.on('close', (resCode, des) => {
-        console.log('connection closed')
-        connections.splice(connections.indexOf(connection), 1)
-    })
-
+app.get('/', function (requisicao, resposta) {
+    resposta.sendFile(__dirname + '/index.html')
 })
+
+
+serverSocket.on('connect', function(socket){
+    socket.on('login', function (nickname) {
+        socket.nickname = nickname
+        const msg = nickname + ' conectou'
+        console.log(msg)
+        serverSocket.emit('chat msg', msg)
+    })
+
+
+    socket.on('disconnect', function(){
+        console.log('Cliente desconectado: ' + socket.nickname)
+    })
+        
+    socket.on('chat msg', function(msg){
+        serverSocket.emit('chat msg', `${socket.nickname} diz: ${msg}`)
+    })
+
+    socket.on('status', function(msg){
+        console.log(msg)
+        socket.broadcast.emit('status', msg)
+    })
+})
+
